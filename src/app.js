f3a00b6cccf55be80f6af5482f6599715492eb4a
NeelahAgarwal <agarwalneelabh3@gmail.com>
2024-03-24 15:42:06 +0530

    Final setup and config

--- src/app.js
+++ src/app.js
@@ -12,4 +12,16 @@ app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({extended:true,limit:'16kb'}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+// Routes import 
+
+import userRoutes from "./routes/user.routes.js"
+// Routes Declaration
+
+app.use("/api/v1/users",userRoutes)
+
+//http://localhost:8000/api/v1/users/register
+
+
 export {app};
